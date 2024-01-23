6e507c801adb376820a0e27f9518122fe97ba0b4
Preetam Bhosle <preetb123@gmail.com>
2024-01-23 20:35:55 +0530

    fixed issues with swift delegates

--- shared/src/iosMain/kotlin/com/example/addlibs/AgoraRTC.ios.kt
+++ shared/src/iosMain/kotlin/com/example/addlibs/AgoraRTC.ios.kt
@@ -1,12 +1,9 @@
 package com.example.addlibs
 
-import cocoapods.AgoraRtcEngine_iOS.AgoraChannelProfile
 import cocoapods.AgoraRtcEngine_iOS.AgoraChannelStats
-import cocoapods.AgoraRtcEngine_iOS.AgoraClientRoleBroadcaster
 import cocoapods.AgoraRtcEngine_iOS.AgoraRtcChannelMediaOptions
 import cocoapods.AgoraRtcEngine_iOS.AgoraRtcEngineDelegateProtocol
 import cocoapods.AgoraRtcEngine_iOS.AgoraRtcEngineKit
-import cocoapods.AgoraRtcEngine_iOS.AgoraUserOfflineReason
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,28 +14,26 @@ import platform.darwin.NSUInteger
 
 class AgoraRtcIOS: AgoraRTC {
 
-    private lateinit var rtcEngine: AgoraRtcEngineKit
+    private lateinit var rtcEngineKit: AgoraRtcEngineKit
     override fun initialize(appId: String): Flow<RTCEngineEvent> = callbackFlow {
-        val delegate: AgoraRtcEngineDelegateProtocol = object : AgoraRtcEngineDelegateProtocol, NSObject() {
+        val delegate : AgoraRtcEngineDelegateProtocol = object : AgoraRtcEngineDelegateProtocol, NSObject() {
             override fun rtcEngine(
                 engine: AgoraRtcEngineKit,
-                channel: String,
+                didJoinChannel: String,
                 withUid: NSUInteger,
                 elapsed: NSInteger
             ) {
-                super.rtcEngine(engine, channel, withUid, elapsed)
-                joinChannelFlow.tryEmit(JoinChannelListenerEvent.OnJoinSuccess(channel, withUid.toInt(), elapsed.toInt()))
+                joinChannelFlow.tryEmit(JoinChannelListenerEvent.OnJoinSuccess(didJoinChannel, withUid.toInt(), elapsed.toInt()))
             }
 
             override fun rtcEngine(
                 engine: AgoraRtcEngineKit,
-                stats: AgoraChannelStats
+                didLeaveChannelWithStats: AgoraChannelStats
             ) {
-                super.rtcEngine(engine, stats)
-                leaveChannelFlow.tryEmit(LeaveChannelListenerEvent.OnLeaveChannelSuccess(CallStats(stats.duration.toInt())))
+                leaveChannelFlow.tryEmit(LeaveChannelListenerEvent.OnLeaveChannelSuccess(CallStats(didLeaveChannelWithStats.duration.toInt())))
             }
         }
-        AgoraRtcEngineKit.sharedEngineWithAppId(appId, delegate)
+        rtcEngineKit = AgoraRtcEngineKit.sharedEngineWithAppId(appId, delegate)
 
         awaitClose {  }
     }
@@ -49,7 +44,7 @@ class AgoraRtcIOS: AgoraRTC {
     override fun joinChannel(token: String, channelId: String, agoraId: Int): Flow<JoinChannelListenerEvent> {
         joinChannelFlow = MutableStateFlow(JoinChannelListenerEvent.OnJoinChannelCalled)
         val options = AgoraRtcChannelMediaOptions()
-        val res = rtcEngine.joinChannelByToken(token, channelId, "", null)
+        val res = rtcEngineKit.joinChannelByToken(token, channelId, "", null)
         if(res < 0) {
             joinChannelFlow.tryEmit(JoinChannelListenerEvent.OnJoinChannelError(res, ""))
         }
@@ -58,7 +53,7 @@ class AgoraRtcIOS: AgoraRTC {
 
     override fun leaveChannel(): Flow<LeaveChannelListenerEvent> {
         leaveChannelFlow = MutableStateFlow(LeaveChannelListenerEvent.OnLeaveChannelInitiated)
-        val res = rtcEngine.leaveChannel(null)
+        val res = rtcEngineKit.leaveChannel(null)
         if(res < 0){
             leaveChannelFlow.tryEmit(LeaveChannelListenerEvent.OnLeaveChannelError(res, ""))
         }
